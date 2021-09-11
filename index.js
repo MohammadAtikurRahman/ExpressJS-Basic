2dd4885cf1b861ed84d70db98ad73b920e01138f
Md. Atikur  Rahman <contact.atikurrahman@gmail.com>
2021-09-11 23:45:05 +0600

    ok

--- index.js
+++ index.js
@@ -1,35 +1,94 @@
 const express  = require ('express');
 const app  = express ();
-//server creation
-
-
 //GET -  Read data
 //Post - Add data
 //Delete- Delete Data
 //Put-   Full Updated Data
 //Patch- Specific Data Updated
+let notes = [
+     { 
+            id:  1,
+            title: 'notetitle1',
+            description: 'Note description 1'
+     },
+     {
+            id: 2,
+            title: 'notetitle2',
+            description: 'Note description 2'
+     }
+]
+//Home Routes
 app.get('/', (req,res) => {
-       res.send('Home routes is Happen Here ');
+
+       res.send('Notes App Home Routes');
 })
-app.get('/signin', (req,res) => {
-       const atik = {  
-              name: 'Md. Atikur Rahman',
-              address: 'Dhaka',
-              age: 30
-       }
-       res.send(atik);
-} )
+//Notes Route
+app.get('/notes', (req,res) => {
+  // This is from array object which i declared'  
+       // res.send(notes);
+})
 
-
-app.get('/hello/:name', (req,res) => {
+//get single notes
+app.get('/notes/:noteId' , (req,res) => {
+ //this noteId from browser
+ //noteId variable routingg
+ const noteId = parseInt(req.params.noteId ) ;
+  //parseint because we get string from link but you need number thats why we converted it
+  // noteId from const is just varible
+  //noteId from params is routing varible
+  // both are same name
+  //const noteid can be changed if i wish
+  //................................
+  // now mathcing note id which i given in the link and which i set in the array object
      
-       const name = req.params.name;
-    
-       res.send(`Hello ${name}`);
-
-
+  const note = notes.find(note => note.id === noteId )
+  res.send(note);
 
 })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// app.get('/signin', (req,res) => {
+//        const atik = {  
+//               name: 'Md. Atikur Rahman',
+//               address: 'Dhaka',
+//               age: 30
+//        }
+//        res.send(atik);
+// } )
+
+
+// app.get('/hello/:name', (req,res) => {
+     
+//        const name = req.params.name;
+    
+//        res.send(`Hello ${name}`);
+
+
+
+// })
 
 
 
@@ -41,6 +100,8 @@ app.get('*', (req,res) => {
 
 
 })
+
+//server creation
 app.listen(3000, ( req,res) => {
 
     console.log(` Server is running at PORT 3000`);
