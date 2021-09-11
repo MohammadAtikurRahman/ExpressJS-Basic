c5de084fcc9d8f642c25c519b4a6747258196312
Md. Atikur  Rahman <contact.atikurrahman@gmail.com>
2021-09-11 13:35:17 +0600

    add

--- index.js
+++ index.js
@@ -1,13 +1,26 @@
 const express  = require ('express');
 const app  = express ();
 //server creation
-
 app.get('/', (req,res) => {
-
        res.send('Home routes is Happen Here ');
+})
+app.get('/signin', (req,res) => {
+       const atik = {  
+              name: 'Md. Atikur Rahman',
+              address: 'Dhaka',
+              age: 30
+       }
+       res.send(atik);
+} )
+
+// for unknown url
+app.get('*', (req,res) => {
+
+   res.send('404 Not Found');
+   console.log('404 Not Found');
+
 
 })
-
 app.listen(3000, ( req,res) => {
 
     console.log(` Server is running at PORT 3000`);
